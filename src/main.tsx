6221258998862100f214040f166e06f93b79ae78
Ran An <ran.an@lilly.com>
2025-12-17 14:49:47 -0500

    Add WebGL detection and helpful error messages for unsupported browsers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import ErrorBoundary from './components/ErrorBoundary.tsx'
+import WebGLCheck from './components/WebGLCheck.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <WebGLCheck>
+        <App />
+      </WebGLCheck>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
